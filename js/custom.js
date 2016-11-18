dde4354e65206eeab4e8cd088485faab0894e59e
misschohoh <lindsay.bradford@gmail.com>
2016-11-17 22:13:24 -0800

    beautiful html

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,21 @@
 /* write script to toggle class on scroll */
-
-     $(window).scroll(function() {
-      if ($(this).scrollTop() > 1){
-      $('header').addClass("sticky");
-      $('.logo').addClass("sticky");
-      }
-      else{
-      $('header').removeClass("sticky")
-      $('.logo').removeClass("sticky")
-      }
-      });
-
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){
+$('header').addClass("sticky");
+$('.logo').addClass("sticky");
+}
+else{
+$('header').removeClass("sticky")
+$('.logo').removeClass("sticky")
+}
+});
 /* og-expander */
-        $('body').click(function(evt){
-      if(evt.target.class== "og-expander")
-      return;
-      // For descendants of menu_content being clicked, remove this
-      // check if you do not want to put constraint on descendants.
-      if($(evt.target).closest('.og-expander').length)
-      return;
-      $(".og-close").trigger("click");
-      });
+$('body').click(function(evt){
+if(evt.target.class== "og-expander")
+return;
+// For descendants of menu_content being clicked, remove this
+// check if you do not want to put constraint on descendants.
+if($(evt.target).closest('.og-expander').length)
+return;
+$(".og-close").trigger("click");
+});
