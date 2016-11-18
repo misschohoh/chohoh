662338e550ff2ac9e4260002e18b1828f1a6b85f
lindsay bradford <lindsay.bradford@gmail.com>
2016-11-17 20:50:59 -0800

    uploading files

---
+++ js/elastic-items.js
@@ -0,0 +1,391 @@
+
+      $(function(){
+      $("#elastic_grid").elastic_grid({
+      'showAllText' : 'ALL',
+      'items' :
+      [
+      
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.adaptaspace.com/' }
+      ],
+      'title'         : 'Adaptaspace',
+      'description'   : 'Adaptaspace&trade; caters to the needs of 24/7 operational facilities, supplying quality and innovation through our console workstation furniture solutions and complementing control room accessories. I worked on the redesign of their website.',
+      'description2'   : 'Tools: HTML, CSS, Javascript.',
+      'thumbnail'     : ['images/Small/adaptaspace01.jpg', 'images/Small/adaptaspace02.jpg'],
+      'large'         : ['images/Large/adaptaspace01.jpg', 'images/Large/adaptaspace02.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Adaptaspace', 'url' : 'http://www.adaptaspace.com/' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.arcteryx.com' }
+      ],
+      'title'         : 'Arc&apos;teryx',
+      'description'   : 'Arcteryx is a high-end clothing company catered to Outdoor Adventurists. I developed layouts, design and functionality throughout the site. Implementing new strategies for client side functionality, responsive and fluid layouts.',
+      'description2'   : 'Tools: HTML5, CSS3, LESS, Javascript, JQuery, JSON, GIT, XML, GRUNT',
+      'thumbnail'     : ['images/Small/arcteryx01.jpg', 'images/Small/arcteryx02.jpg', 'images/Small/arcteryx03.jpg', 'images/Small/arcteryx04.jpg'],
+      'large'         : ['images/Large/arcteryx01.jpg', 'images/Large/arcteryx02.jpg', 'images/Large/arcteryx03.jpg', 'images/Large/arcteryx04.jpg'],
+      'button_list'   :
+      [
+      
+      { 'title':'Footwear', 'url':'http://www.arcteryx.com/LearnMore.aspx?country=ca&language=en&article=Footwear&intcmp=find_learn_Footwear'},
+      { 'title':'Gift guide ', 'url':'http://www.arcteryx.com/LearnMore.aspx?country=ca&language=en&article=GiftGuide&intcmp=Nav-Shop_shop_Gift-Guide'},
+      { 'title':'Veilance', 'url':'http://veilance.arcteryx.com/Home.aspx?language=EN'},
+      { 'title':'Leaf', 'url' : 'http://leaf.arcteryx.com/' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.atlasmind.com/' }
+      ],
+      'title'         : 'Atlas Mind',
+      'description'   : 'Worked on a the redesign and updates for various sites. Developed the mobile site for Mandalay Entertainment',
+      'description2'   : 'Tools: HTML, CSS, JavaScript, JQuery, Mobile Development',
+      'thumbnail'     : ['images/Small/i.ko03.jpg', 'images/Small/i.ko02.jpg', 'images/Small/i.ko01.jpg'],
+      'large'         : ['images/Large/i.ko03.jpg', 'images/Large/i.ko02.jpg', 'images/Large/i.ko01.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Mandalay', 'url' : 'http://www.mandalay.com' },
+      { 'title':'Bao Dimsum', 'url':'http://www.baodimsum.com/'}
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://tinyurl.com/za75otu' }
+      ],
+      'title'         : 'Cimarron Group',
+      'description'   : 'The Cimarron Group was a Entertainment Marketing and Ad Agency. I developed sites for movies and video games. I was given PSDs to develop into small teaser sites for films that would function on mobile devices.',
+      'description2'   : 'Tools: HTML, CSS, JavaScript, JQuery, Mobile Development, Drupal',
+      'thumbnail'     : ['images/Small/cimmaron01.jpg', 'images/Small/cimmaron02.jpg', 'images/Small/cimmaron03.jpg', 'images/Small/cimmaron04.jpg'],
+      'large'         : ['images/Large/cimmaron01.jpg', 'images/Large/cimmaron02.jpg', 'images/Large/cimmaron03.jpg', 'images/Large/cimmaron04.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Bioshock Infinite', 'url' : 'http://www.bioshockinfinite.com/home' },
+      { 'title':'The Change Up', 'url' : 'http://chohoh.com/cim/CU/' },
+      { 'title':'Bioshock Infinite', 'url' : 'http://www.bioshockinfinite.com/home' },
+      { 'title':'How She Does It', 'url':'http://chohoh.com/cim/IDK/'}
+      ],
+      'tags'          : ['WEBWORK, DRUPAL']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'https://www.linkedin.com/company/creative-asylum' }
+      ],
+      'title'         : 'Creative Asylum',
+      'description'   : 'Creative Asylum is an Emmy-Award winning, global integrated agency that works behind the scenes for some of the largest ad agencies, entertainment studios and brands in the world. I worked on the website build for International Sey Ladies Campaign',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, Flash',
+      'thumbnail'     : ['images/Small/creativeasylum01.jpg'],
+      'large'         : ['images/Large/creativeasylum01.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Sexy Ladies International', 'url' : 'http://www.g4tv.com/sexyladies/' },
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://digitallionmarketing.com/' }
+      ],
+      'title'         : 'Digital Lion Marketing',
+      'description'   : 'Digital Lion Marketing creates custom websites, incentive programs, promotions, sweepstakes, and email campaigns. I worked on developing the redesign ad wireframe for various t.v. network scheduling sites',
+      'description2'  : 'Tools: HTML5, CSS3, JavaScript, JQuery, wireframes',
+      'thumbnail'     : ['images/Small/digitallion01.jpg', 'images/Small/digitallion02.jpg', 'images/Small/digitallion03.jpg'],
+      'large'         : ['images/Large/digitallion01.jpg', 'images/Large/digitallion02.jpg', 'images/Large/digitallion03.jpg'],
+      'button_list'   :
+      [
+      { 'title':'TV Guide', 'url' : 'http://chohoh.com/tvguide/'},
+      { 'title':'FX/Dish', 'url':'http://chohoh.com/fxdish/'}
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.eonline.com/' }
+      ],
+      'title'         : 'E! Online',
+      'description'   : 'Designed and developed online production for E! Editorial and programming. Interactive marketing campaigns. (Microsites, sweepstakes, and ad units). Intranet and extranet updates. Daily communication and coordination with marketing, ad sales, editorial, and creative services.',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, PHP, PSD to HTML, custom CMS',
+      'thumbnail'     : ['images/Small/eonline01.jpg'],
+      'large'         : ['images/Large/eonline01.jpg'],
+      'button_list'   :
+      [
+      { 'title':'E Online', 'url' : 'http://www.eonline.com/' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.ericreussmd.com/' }
+      ],
+      'title'         : 'Eric Reuss MD',
+      'description'   : 'I worked on the design and development for Scottsdale Gynecology and Obstertrics. They were looking for something simple and easy on navigate and use for aptients to check in and download paperwork',
+      'description2'  : 'Tools: HTML5, CSS3, JavaScript, JQuery, Site Design, Responsive Development',
+      'thumbnail'     : ['images/Small/ericreuss01.jpg'],
+      'large'         : ['images/Large/ericreuss01.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Eric Reuss MD', 'url' : 'http://www.ericreussmd.com/' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://freespiritproductions.com/' }
+      ],
+      'title'         : 'FreeSpirit Productions',
+      'description'   : 'I worked on the design ad devlopment for this wordpress site. Free Spirit Productions was looking for a simple solution to display their work, and easy for them to update.',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, Site Design, Responsive Development, Wordpress',
+      'thumbnail'     : ['images/Small/freespirit01.jpg' ],
+      'large'         : ['images/Large/freespirit01.jpg' ],
+      'button_list'   :
+      [
+      { 'title':'FreeSpirit Productions', 'url' : 'http://freespiritproductions.com/' }
+      ],
+      'tags'          : ['WEBWORK, WORDPRESS']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.intel.com' }
+      ],
+      'title'         : 'Intel',
+      'description'   : 'Worked on developing and updating content on Intels website.',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, Documentum',
+      'thumbnail'     : ['images/Small/intel01.jpg'],
+      'large'         : ['images/Large/intel01.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Intel', 'url' : 'http://www.intel.com' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.jillianlove.com/' }
+      ],
+      'title'         : 'Jillian Love',
+      'description'   : 'Jillian Love is a talented and certified  raw vegan chef &#38; health coach who creates engaging sensory experiences with the fusion of food, entertainment and fun. I worked on the design and development this Wordpress site',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, Site Design, Responsive Development, Wordpress',
+      'thumbnail'     : ['images/Small/jillianlove02.jpg' ],
+      'large'         : ['images/Large/jillianlove02.jpg' ],
+      'button_list'   :
+      [
+      { 'title':'Jillian Love', 'url' : 'http://www.jillianlove.com/' }
+      ],
+      'tags'          : ['WEBWORK, WORDPRESS']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'https://www.kabbalah.com/' }
+      ],
+      'title'         : 'Kabbalah Center',
+      'description'   : 'Kabbalah is an ancient wisdom that provides practical tools for creating joy and lasting fulfillment. I worked updating content and building out new content for their custom CMS',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, PHP, custom CMS',
+      'thumbnail'     : ['images/Small/kaballah01.jpg' ],
+      'large'         : ['images/Large/kaballah01.jpg' ],
+      'button_list'   :
+      [
+      { 'title':'Kabbalah Center', 'url' : 'https://www.kabbalah.com/' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.mikebrucefilms.com/news/' }
+      ],
+      'title'         : 'Mike Bruce Films',
+      'description'   : 'Mike Bruce is an American film director, writer and producer. I worked on the design and development this Wordpress site',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, Site Design, Responsive Development, Wordpress',
+      'thumbnail'     : ['images/Small/mikebruce01.jpg' ],
+      'large'         : ['images/Large/mikebruce01.jpg' ],
+      'button_list'   :
+      [
+      { 'title':'Mike Bruce Films', 'url' : 'http://www.mikebrucefilms.com/news/' }
+      ],
+      'tags'          : ['WEBWORK, WORDPRESS']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://myhomeopathiccenter.com/' }
+      ],
+      'title'         : 'My Homeopathic Center',
+      'description'   : 'Dr. Yosef Haridim specializes in Classical Homeopathy and determining your Individual Essence of Health and Wellness. I worked on the design and development for this simple one page informational site.',
+      'description2'  : 'Tools: HTML5, CSS, JQuery, Site Design, Responsive Development',
+      'thumbnail'     : ['images/Small/myhomepathic.jpg'],
+      'large'         : ['images/Large/myhomepathic.jpg'],
+      'button_list'   :
+      [
+      { 'title':'My Homeopathic Center', 'url' : 'http://myhomeopathiccenter.com/' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://theneuralclinic.com/' }
+      ],
+      'title'         : 'The Neural Clinic',
+      'description'   : 'PTSD and Trauma Related Services',
+      'description2'  : 'Tools: HTML5, CSS, Wordpress, JQuery, Site Design, Responsive Development',
+      'thumbnail'     : ['images/Small/neuralclinic01.jpg'],
+      'large'         : ['images/Large/neuralclinic01.jpg'],
+      'button_list'   :
+      [
+      { 'title':'The Neural Clinic', 'url' : 'theneuralclinic.com/' }
+      ],
+      'tags'          : ['WEBWORK, WORDPRESS']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.pinch.nu' }
+      ],
+      'title'         : 'Pinch',
+      'description'   : 'Pinch is a strategic design and brand development firm, based in Portland, Oregon. I worked on various contract projects doing PSD to html layouts utilizing my development skills',
+      'description2'  : 'Tools: HTML, xHTML, CSS, JQuery, JavaScript, Responsive Development',
+      'thumbnail'     : ['images/Small/pinch02.jpg', 'images/Small/pinch03.jpg'],
+      'large'         : ['images/Large/pinch02.jpg', 'images/Large/pinch03.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Oregon Humanities', 'url' : 'http://oregonhumanities.org/' },
+      { 'title':'Portland Center Stage', 'url':'https://www.pcs.org/'}
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.therefinerycreative.com/' }
+      ],
+      'title'         : 'The Refinery Creative',
+      'description'   : 'The Refinery is a creative ad agency specializing in print and a/v advertising with a focus on the entertainment industry. I worked on development and designing various interactive online campaigns',
+      'description2'   : 'Tools: HTML5, CSS3, JQuery, Javascript, PHP, Responsive Development, PSD to development',
+      'thumbnail'     : ['images/Small/refinery01.jpg'],
+      'large'         : ['images/Large/refinery01.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Refinery Interactive', 'url' : 'http://www.therefinerycreative.com/#!/interactive' }
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.sarkissianpartners.com/' }
+      ],
+      'title'         : 'Sarkissian Mason',
+      'description'   : 'Sarkissian Mason is and interactive agency in New York. I worked on thedevelopment of Mazda USA and Ford websites',
+      'description2'  : 'Tools: HTML5, CSS3, JQuery, Javascript, PHP, Responsive Development, PSD to development',
+      'thumbnail'     : ['images/Small/sarkissianmason01.jpg', 'images/Small/sarkissianmason02.jpg'],
+      'large'         : ['images/Large/sarkissianmason01.jpg', 'images/Large/sarkissianmason02.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Mazda USA', 'url':'https://www.mazdausa.com/'}
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://www.sonypictures.com' }
+      ],
+      'title'         : 'Sony Interactive',
+      'description'   : 'Worked on Sony Interactive movie websites updating content, developing sites, implementing functionality and and social media content.',
+      'description2'  : 'Tools: HTML5, CSS3, JQuery, Javascript, PHP, GIT, JSON, PSD to development',
+      'thumbnail'     : ['images/Small/sony01.jpg', 'images/Small/sony02.jpg', 'images/Small/sony03.jpg', ],
+      'large'         : ['images/Large/sony01.jpg', 'images/Large/sony02.jpg', 'images/Large/sony03.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Sony Interactive', 'url' : 'http://tinyurl.com/zlz3rqa' },
+      { 'title':'Greener World', 'url':'http://www.sonypicturesgreenerworld.com/'}
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'https://www.sunbowl.ca/' }
+      ],
+      'title'         : 'Sunbowl Systems',
+      'description'   : 'Sunbowl Systems is an agency that builds out Wordpress sites for clients and sometimes utilizing shopify to sell products. I worked on updates and redesign of various wordpress sites.',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, Wordpress, PHP, Responsive Development',
+      'thumbnail'     : ['images/Small/sunbowl01.jpg' ],
+      'large'         : ['images/Large/sunbowl01.jpg' ],
+      'button_list'   :
+      [
+      { 'title':'Sunbowl Systems', 'url' : 'https://www.sunbowl.ca/' }
+      ],
+      'tags'          : ['WEBWORK, WORDPRESS']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'https://www.trailerpark.com/' }
+      ],
+      'title'         : 'Trailer Park',
+      'description'   : 'Trailer Park is an Interactive Entertainment Ad Agency. I worked on the redesign of the Oscars website which is developed in Drupal content management',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, Drupal, PHP, Responsive Development',
+      'thumbnail'     : ['images/Small/trailerpark01.jpg', 'images/Small/trailerpark02.jpg'],
+      'large'         : ['images/Large/trailerpark01.jpg', 'images/Large/trailerpark02.jpg'],
+      'button_list'   :
+      [
+      { 'title':'Oscars', 'url':'http://www.oscars.org/'}
+      ],
+      'tags'          : ['WEBWORK, DRUPAL']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://valveinteractive.com/' }
+      ],
+      'title'         : 'Valve Interactive',
+      'description'   : 'Northwests leading integrated digital agency. I worked on various contract web development projects. ',
+      'description2'  : 'Tools: HTML, CSS, JavaScript, JQuery, PHP, Responsive Development',
+      'thumbnail'     : ['images/Small/valve01.jpg', 'images/Small/valve02.jpg' ],
+      'large'         : ['images/Large/valve01.jpg', 'images/Large/valve02.jpg' ],
+      'button_list'   :
+      [
+      { 'title':'Food IPC', 'url':'http://chohoh.com/foodipc/'}
+      ],
+      'tags'          : ['WEBWORK']
+      },
+      {
+      'icon_list'   :
+      [
+      { 'url' : 'http://visualade.com/' }
+      ],
+      'title'         : 'Visualade',
+      'description'   : 'Visualade is an award winning design and interactive agency with a passion for forward-thinking strategy and technology. I worked on the development of various projects',
+      'description2'  : 'Tools: HTML, CSS,, LESS, JavaScript, JQuery, PHP, JSON, Responsive Development',
+      'thumbnail'     : ['images/Small/visualade01.jpg', 'images/Small/visualade02.jpg', 'images/Small/visualade03.jpg', 'images/Small/visualade04.jpg' ],
+      'large'         : ['images/Large/visualade01.jpg', 'images/Large/visualade02.jpg', 'images/Large/visualade03.jpg', 'images/Large/visualade04.jpg' ],
+      'button_list'   :
+      [
+      { 'title':'Mall of America', 'url' : 'https://www.mallofamerica.com/' },
+      { 'title':'South Coast Plaza', 'url':'http://www.southcoastplaza.com/'},
+      { 'title':'The Program', 'url':'http://tinyurl.com/jh33hp7'}
+      ],
+      'tags'          : ['WEBWORK']
+      }
+      ]
+      });
+      });
